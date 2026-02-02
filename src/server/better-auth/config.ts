858ee8fc868728240ab1002b0a20bec6349dbeb2
Lok Lok <yellowhappy831@gmail.com>
2026-02-02 13:44:34 +0800

    save

--- src/server/better-auth/config.ts
+++ src/server/better-auth/config.ts
@@ -13,7 +13,7 @@ export const auth = betterAuth({
     provider: "mongodb", // or "sqlite" or "mysql"
   }),
   emailAndPassword: {
-    enabled: false,
+    enabled: true,
   },
   socialProviders: {
     google: {
