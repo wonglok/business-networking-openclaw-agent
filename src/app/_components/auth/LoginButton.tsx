95de7f7285492050bee6fede58c3547ef3f7420c
Lok Lok <yellowhappy831@gmail.com>
2026-02-03 14:55:01 +0800

    save

--- src/app/_components/auth/LoginButton.tsx
+++ src/app/_components/auth/LoginButton.tsx
@@ -10,6 +10,7 @@ export function LoginButton() {
         onClick={async () => {
           const response = await authClient.signIn.social({
             provider: 'google',
+            disableRedirect: true,
             // callbackURL: `/api/auth/callback/google`,
             // scopes: [
             //   "https://www.googleapis.com/auth/userinfo.email",
