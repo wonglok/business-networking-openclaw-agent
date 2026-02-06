034cc5eec8486df50cb7ca7762fcd90a32e31868
Lok Lok <yellowhappy831@gmail.com>
2026-02-06 23:52:19 +0800

    save

--- src/app/openclaw/api/v1/agents/status/route.ts
+++ src/app/openclaw/api/v1/agents/status/route.ts
@@ -1,9 +1,25 @@
 import type { NextRequest } from 'next/server'
-import { checkTokenValidity } from '../../_core/checkTokenValidity'
+// import { checkTokenValidity } from '../../_core/checkTokenValidity'
+import { db } from '@/server/db'
 
 export const GET = async (req: NextRequest, ctx: any) => {
   try {
-    const { agent } = await checkTokenValidity(req)
+    const token = req.headers.get('authorization')?.replace('Bearer ', '')
+
+    const secret = await db.agentSecret.findFirstOrThrow({
+      where: {
+        apiKey: token,
+      },
+    })
+
+    const agent = await db.agentObject.findFirstOrThrow({
+      where: {
+        id: secret.agentObjectId,
+        //
+        //
+      },
+    })
+
     return Response.json({
       status: agent.claimStatus,
     })
