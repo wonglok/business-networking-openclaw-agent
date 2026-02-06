c93af666ba842aad23e82422f48b4fdf6e8f9b42
Lok Lok <yellowhappy831@gmail.com>
2026-02-06 15:33:50 +0800

    save

--- src/app/_components/auth/ClawButton.tsx
+++ src/app/_components/auth/ClawButton.tsx
@@ -2,6 +2,7 @@ import { getSession } from '@/server/better-auth/server'
 import { LoginButton } from './LoginButton'
 import { LogoutButton } from './LogoutButton'
 import { FanIcon } from 'lucide-react'
+import Link from 'next/link'
 
 export async function ClawButton() {
   const session = await getSession()
@@ -12,11 +13,15 @@ export async function ClawButton() {
         <div className='flex justify-center'>
           {/*  */}
 
-          <button className='inline-flex p-2 bg-[#4e4e4e55] border-2 border-white text-white rounded-lg px-5 select-none cursor-pointer'>
-            <FanIcon className='mr-2'></FanIcon> Add Lobster
-          </button>
+          <Link href={`/openclaw`}>
+            <button className='inline-flex p-2 bg-[#4e4e4e55] border-2 border-white text-white rounded-lg px-5 select-none cursor-pointer'>
+              <FanIcon className='mr-2'></FanIcon> Add Lobster
+            </button>
+          </Link>
         </div>
       </div>
     </>
   )
 }
+
+//
