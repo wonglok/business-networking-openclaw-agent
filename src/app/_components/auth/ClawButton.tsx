4ab235d2e7206800bedfcf1a33e3cb97b5804960
Lok Lok <yellowhappy831@gmail.com>
2026-02-06 15:34:57 +0800

    claw button

--- src/app/_components/auth/ClawButton.tsx
+++ src/app/_components/auth/ClawButton.tsx
@@ -1,6 +1,6 @@
 import { getSession } from '@/server/better-auth/server'
-import { LoginButton } from './LoginButton'
-import { LogoutButton } from './LogoutButton'
+// import { LoginButton } from './LoginButton'
+// import { LogoutButton } from './LogoutButton'
 import { FanIcon } from 'lucide-react'
 import Link from 'next/link'
 
